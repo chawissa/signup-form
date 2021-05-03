a7d2c38259244019a883adb5bbf12fe0156ccc28
Charissa Ramirez <chawissa@gmail.com>
2021-05-03 07:29:08 -0400

    README.md updated

--- script.js
+++ script.js
@@ -37,3 +37,22 @@ function validateEmail(email) {
 //   const pword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()+=-\?;,./{}|\":<>\[\]\\\' ~_]).{8,}/;
 //   return pword.test(password);
 // }
+
+// function validatePassword() {
+//   var p = document.getElementById("newPassword").value,
+//     errors = [];
+//   if (p.length < 8) {
+//     errors.push("Your password must be at least 8 characters");
+//   }
+//   if (p.search(/[a-z]/i) < 0) {
+//     errors.push("Your password must contain at least one letter.");
+//   }
+//   if (p.search(/[0-9]/) < 0) {
+//     errors.push("Your password must contain at least one digit.");
+//   }
+//   if (errors.length > 0) {
+//     alert(errors.join("\n"));
+//     return false;
+//   }
+//   return true;
+// }
